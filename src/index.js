d39ade0f1f53f145623d86ebf5c0e4c8e5b05078
Ben Allenden <benallenden@protonmail.com>
2024-02-24 20:28:10 +0000

    Install and setup sass packages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import './style.css'
+import './scss/main.scss'
 
--- webpack.common.js
+++ webpack.common.js
@@ -14,8 +14,8 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
           },
           {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
