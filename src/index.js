2f77cd101476814ef69973f22d93d37b1bedf375
Ben Allenden <benallenden@protonmail.com>
2024-02-26 16:39:30 +0000

    Add JS for theme toggle button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './scss/main.scss'
 
+const themeToggle = document.getElementById('theme-toggle')
+themeToggle.addEventListener('click', toggleTheme)
+
+function toggleTheme() {
+    const html = document.querySelector('html')
+    const currentTheme = html.getAttribute('data-theme')
+
+    if (currentTheme == 'light') {
+        html.setAttribute('data-theme', 'dark')
+    } else {
+        html.setAttribute('data-theme', 'light')
+    }
+}
