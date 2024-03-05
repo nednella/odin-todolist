7f33b3de0e16adb9b37ec3f90327056e5aa32e10
Ben Allenden <benallenden@protonmail.com>
2024-03-05 19:06:50 +0000

    Implement task and project creation functionality

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,12 @@
 import '../scss/app.scss'
+import toDoList from './toDo'
 
 const ui = (() => {
+
+    // Initialise application
+    const toDo = new toDoList()
+
+
 
     // Buttons
     const themeToggle = document.getElementById('theme-toggle')
@@ -15,6 +21,28 @@ const ui = (() => {
     // Elements
     const html = document.querySelector('html')
     const nav = document.getElementById('nav')
+    const projectsList = document.querySelector('.custom-projects')
+    const projectDisplay = document.getElementById('active-project')
+
+    // Variables
+    let projectTitle = document.getElementById('project-title-1').textContent
+    let activeProject = toDo.getProject(projectTitle)
+
+
+
+    // Create some tasks for 'My Day'
+    let myDay = toDo.getProject('My Day')
+    myDay.addTask('My Day - Testing 1')
+    myDay.addTask('My Day - Testing 2')
+    myDay.addTask('My Day - Testing 3')
+
+    // Create some tasks for 'All Tasks'
+    let allTasks = toDo.getProject('All Tasks')
+    allTasks.addTask('All Tasks - Testing 1')
+    allTasks.addTask('All Tasks - Testing 2')
+    allTasks.addTask('All Tasks - Testing 3')
+
+    reloadProject()
 
 
 
@@ -38,15 +66,60 @@ const ui = (() => {
     }
 
     function navSetActive(e) {
-        navRemoveActive()
+        const navOptions = document.querySelectorAll('.nav-item')
+        navOptions.forEach(option => option.classList.remove('active'))
+
         e.target.classList.add('active')
         // toggleNav()                 // Close nav after selecting a nav link
     }
 
-    function navRemoveActive() {
-        const navOptions = document.querySelectorAll('.nav-item')
-        navOptions.forEach(option => option.classList.remove('active'))
+    function navHandleClick(e) {
+        if (e.target && e.target.classList.contains('nav-item')) {
+            const selectedProject = e.target.children[0].children[1].textContent
+            activeProject = toDo.getProject(selectedProject)
+            navSetActive(e)         // Set selected nav link as active
+            reloadProject()         // Populate project display with selected link
+        }
+        if (e.target && e.target.classList.contains('project-delete')) {
+            console.log('delete project')   // Delete selected project
+
+            // If deleted project is active,
+                // Set X as new active project
+            // Reload UI
+
+        }
     }
+
+
+
+
+
+
+    function createTask(Title) {
+        activeProject.addTask(Title)
+        appendTask(Title)
+    }
+
+    function createProject(Title) {
+        toDo.addProject(Title)
+        appendProject(Title)
+    }
+
+    function reloadProject() {
+        clearProject()
+
+        const tasks = activeProject.getTasks()
+        tasks.forEach(task => {
+            appendTask(task.getTitle())
+        })
+    }
+
+    function clearProject() {
+        projectDisplay.innerHTML = ''
+    }
+
+
+
 
 
 
@@ -54,8 +127,7 @@ const ui = (() => {
 
     // Create DOM Elements
     function appendTask(value) {
-        const taskList = document.getElementById('project-tasks')
-        taskList.innerHTML += `
+        projectDisplay.innerHTML += `
             <div class="task">
                 <input 
                     class="task-checkbox"
@@ -67,8 +139,7 @@ const ui = (() => {
     }
 
     function appendProject(value) {
-        const projectList = document.querySelector('.custom-projects')
-        projectList.innerHTML += `
+        projectsList.innerHTML += `
             <span href="" class="nav-item" data-custom-project>
                 <span class="nav-item-left">
                     <span class="material-symbols-rounded">menu</span>
@@ -87,28 +158,12 @@ const ui = (() => {
 
 
 
+
     // Event Listeners
     themeToggle.addEventListener('click', () => toggleTheme())
     navOpen.addEventListener('click', () => toggleNav())
     navClose.addEventListener('click', () => toggleNav())
-    navLinks.addEventListener('click', (e) => {
-        if (e.target && e.target.classList.contains('nav-item')) {
-            navSetActive(e)         // Set selected nav link as active
-
-            // Set nav link as active
-            // Set selected project as active
-            // Reload UI
-
-        }
-        if (e.target && e.target.classList.contains('project-delete')) {
-            console.log('delete project')   // Delete selected project
-
-            // If deleted project is active,
-                // Set X as new active project
-            // Reload UI
-
-        }
-    })
+    navLinks.addEventListener('click', (e) => navHandleClick(e))
     nav.addEventListener('click', (e) => {
         if (e.target.nodeName == 'DIALOG') {
             nav.close()             // Close nav modal by clicking on backdrop
@@ -128,11 +183,11 @@ const ui = (() => {
         }
         if (e.key == 'Enter') {     // Submit input on Enter
             if (document.activeElement == taskInput && taskInput.value !== '') {
-                appendTask(taskInput.value)
+                createTask(taskInput.value)
                 taskInput.value = ''         
             }
             if (document.activeElement == projectInput  && projectInput.value !== '') {
-                appendProject (projectInput.value)
+                createProject(projectInput.value)
                 projectInput.value = ''
             }
         }  
