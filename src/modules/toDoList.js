00791ea364a71b5da561fa1d810afad520c0e359
Ben Allenden <benallenden@protonmail.com>
2024-03-06 21:48:41 +0000

    Expand toDoList() class functionality to store active project

--- src/modules/toDoList.js
+++ src/modules/toDoList.js
@@ -4,13 +4,26 @@ import Task from './Task'
 export default class toDoList {
     constructor() {
         this.list = []
+        this.activeProject = ''
         this.list.push(new Project('my day'))
         this.list.push(new Project('all tasks'))
     }
+    setActiveProject (projectTitle) {
+        return this.activeProject = this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())
+    }
+    getActiveProject () {
+        return this.activeProject
+    }
     addProject (projectTitle) {
-        this.list.push(new Project(projectTitle))
+        if (this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())) return
+        else this.list.push(new Project(projectTitle.toLowerCase()))
+        console.log('New Project: ' + JSON.stringify(this.list[(this.list.length - 1)]))
+        // return this.list.push(new Project(projectTitle.toLowerCase()))
     }
     getProject (projectTitle) {
         return this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())
+    }
+    getProjects () {
+        return this.list
     }
 }
