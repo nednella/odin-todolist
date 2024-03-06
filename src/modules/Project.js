395985046157cc5e7783ea2ab825b2912280fa7a
Ben Allenden <benallenden@protonmail.com>
2024-03-06 22:25:10 +0000

    Implement Project() class ID and refactor any data search for ID

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -3,17 +3,23 @@ import task from './Task'
 export default class project {
     constructor (title) {
         this.title = title
+        this.id = this.title.toLowerCase()
         this.tasks = []
     }
     setTitle (title) {
         this.title = title
+        // this.id = title.toLowerCase() // Change ID to reflect new title
     }
     getTitle () {
         return this.title
     }
+    getID () {
+        return this.id
+    }
     addTask (title, dueDate) {
         this.tasks.push(new task(title, dueDate))
-        console.log('New Task: ' + JSON.stringify(this.tasks[(this.tasks.length - 1)]))
+        // console.log('New Task: ' + JSON.stringify(this.tasks[(this.tasks.length - 1)]))
+        // return this.tasks.push(new task(title, dueDate))
     }
     getTasks () {
         return this.tasks
--- src/modules/toDoList.js
+++ src/modules/toDoList.js
@@ -4,24 +4,25 @@ import Task from './Task'
 export default class toDoList {
     constructor() {
         this.list = []
-        this.activeProject = ''
-        this.list.push(new Project('my day'))
-        this.list.push(new Project('all tasks'))
+        this.list.push(new Project('My Day'))
+        this.list.push(new Project('All Tasks'))
+        this.activeProject = this.list[0]
     }
     setActiveProject (projectTitle) {
-        return this.activeProject = this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())
+        return this.activeProject = this.list.find((project) => project.getID() == projectTitle.toLowerCase())
     }
     getActiveProject () {
         return this.activeProject
     }
     addProject (projectTitle) {
-        if (this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())) return
-        else this.list.push(new Project(projectTitle.toLowerCase()))
-        console.log('New Project: ' + JSON.stringify(this.list[(this.list.length - 1)]))
+        if (this.list.find((project) => project.getID() == projectTitle.toLowerCase())) return console.log('Project already exists.')
+        else this.list.push(new Project(projectTitle))
+
+        // console.log('New Project: ' + JSON.stringify(this.list[(this.list.length - 1)]))
         // return this.list.push(new Project(projectTitle.toLowerCase()))
     }
     getProject (projectTitle) {
-        return this.list.find((title) => title.getTitle() == projectTitle.toLowerCase())
+        return this.list.find((project) => project.getID() == projectTitle.toLowerCase())
     }
     getProjects () {
         return this.list
