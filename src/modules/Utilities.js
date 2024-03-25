201c7ce8a4b492544b7c7325a3c5280ec7ba26a7
Ben Allenden <benallenden@protonmail.com>
2024-03-25 03:53:33 +0000

    Create Utilities module, add date parser

---
+++ src/modules/Utilities.js
@@ -0,0 +1,18 @@
+import { format } from 'date-fns'
+
+export function parseDueDate(date) {
+    // Take format of '2024-01-01'
+    // Return format of '01 Jan 24'
+    // Do not include the year if it is equal to the current year
+
+    const currentYear = format(new Date(), "yyyy"),
+          months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    let parts = date.split('-'),
+        day = parts[2],
+        month = months[Number(parts[1] - 1)],
+        year = parts[0]
+
+    if (year == currentYear) return `${day} ${month}`
+    else return `${day} ${month} ${year}`
+}
